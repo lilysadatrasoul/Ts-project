6e7075e19e8f87ceac27308f9c3767f836afe7b6
Lily Sadat <l.sadatrasoul@gmail.com>
2022-03-01 22:16:01 +0330

    set save button

--- main.js
+++ main.js
@@ -1,36 +1,111 @@
 
 
-let row_number = 1
+ let row_number = 1;
 
 function addRow () {
-  var table = document.getElementById("table");
+  
+       var table = document.getElementById("table");
+       var row = table.insertRow();
 
- var row = table.insertRow();
+       var cell1 = row.insertCell(0);
+       cell1.innerHTML = row_number;
 
- var cell1 = row.insertCell(0);
- cell1.innerHTML = row_number;
- var cell2 = row.insertCell(1);  
- cell2.innerHTML = "";
- var cell3 = row.insertCell(2);  
- cell3.innerHTML = "";
- var cell4 = row.insertCell(3);  
- cell4.innerHTML = "";
- var cell5 = row.insertCell(4);  
- cell5.innerHTML = "";
- var cell6 = row.insertCell(5);  
- cell6.innerHTML = "";
- var cell7 = row.insertCell(6);  
- cell7.innerHTML = "";
+       var cell2 = row.insertCell(1); 
+       var element2 = document.createElement("input");
+			 element2.type = "text";
+			 element2.name = "txtbox[]";
+			 cell2.appendChild(element2);
 
- row_number += 1;
+       var cell3 = row.insertCell(2);  
+        var element3 = document.createElement("input");
+			 element3.type = "text";
+			 element3.name = "txtbox[]";
+			 cell3.appendChild(element3);
 
- if(row_number >= 10)
- {
-   
+       var cell4 = row.insertCell(3);  
+       var element4 = document.createElement("input");
+			 element4.type = "text";
+			 element4.name = "txtbox[]";
+			 cell4.appendChild(element4);
 
- }
+       var cell5 = row.insertCell(4);  
+       var element5= document.createElement("input");
+			 element5.type = "text";
+			 element5.name = "txtbox[]";
+			 cell5.appendChild(element5);
+
+
+       var cell6 = row.insertCell(5);
+       var element6 = document.createElement("button");
+			 element6.type = "button";
+			 element6.name="edit";
+       element6.innerHTML = "ویرایش";
+       element6.style.width=`50px`;
+       element6.style.height=`20px`;
+			 cell6.appendChild(element6);  
+
+       var cell7 = row.insertCell(6); 
+       var element7 = document.createElement("button");
+			 element7.type = "button";
+			 element7.name="delete";
+       element7.innerHTML = "حذف";
+       element7.style.width=`50px`;
+       element7.style.height=`20px`;
+			 cell7.appendChild(element7);
+
+       var cell8 = row.insertCell(7);  
+       var element8= document.createElement("button");
+			 element8.type = "button";
+			 element8.name = "save";
+       element8.innerHTML = "ذخیره";
+       element8.style.width=`50px`;
+       element8.style.height=`20px`;
+			 cell8.appendChild(element8); 
+      
+  row_number += 1;
+
+
+
+
+  element8.addEventListener('click', () => {
+    
+    let person= {
+      name :cell2.innerHTML,
+      country : cell3.innerHTML,
+      age: cell4.innerHTML,
+      city :cell5.innerHTML,
+    }
+    console.log(person)
+  });
+
+
 }
 
+
+
+var cell = document.getElementsByTagName('td');
+for (var i=2; i<6; i++) {
+   cell[i].onclick = function() {
+      var data = this.getAttribute('data-cell');
+      editEvent = document.getElementById('addevent');//The pop-up window
+      editEvent.style.cssText ='display: block;';
+      this.style.position = 'relative';
+
+      this.innerHTML = " "//??? I want the inputs data to be inserted here
+
+
+
+
+
+// class person {
+//   constructor(name, city, country, age) {
+//      this.Name = document.getElementById(`name`);
+//      this.city = document.getElementById(`city`);
+//      this.country = document.getElementById(`country`);
+//      this.age = document.getElementById(`age`);
+//   }
+
+// }
 
 
 
