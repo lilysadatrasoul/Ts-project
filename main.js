98777b953bea8852201479ca349cc933095899e4
Lily Sadat <l.sadatrasoul@gmail.com>
2022-04-09 00:43:15 +0430

    Implement pagination

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-
-
 //  let row_number = 1;
 
 // function addRow () {
-  
+
 //        var table = document.getElementById("table");
 //        var row = table.insertRow();
 
@@ -61,22 +59,22 @@
 //        element8.style.width=`50px`;
 //        element8.style.height=`20px`;
 // 			 cell8.appendChild(element8); 
-      
+
 //   row_number += 1;
 
 
 
 
-  // element8.addEventListener('click', () => {
-    
-  //   let person= {
-  //     name :cell2.innerHTML,
-  //     country : cell3.innerHTML,
-  //     age: cell4.innerHTML,
-  //     city :cell5.innerHTML,
-  //   }
-  //   console.log(person)
-  // });
+// element8.addEventListener('click', () => {
+
+//   let person= {
+//     name :cell2.innerHTML,
+//     country : cell3.innerHTML,
+//     age: cell4.innerHTML,
+//     city :cell5.innerHTML,
+//   }
+//   console.log(person)
+// });
 
 
 // }
@@ -110,32 +108,117 @@
 
 
 
-const formElement= document.querySelector("form")
-const tbodyElement= document.querySelector("tbody")
+const formElement = document.querySelector("form")
+const tbodyElement = document.querySelector("tbody")
 const tableElement = document.querySelector(`table`)
-let data=[]
-let rowNumber=1
+let prePage = document.getElementById(`previous`)
+let nexPage = document.getElementById(`next`)
+let data = []
+let rowNumber = 0
+let pageNumber = 0
 
-function addingProfile(e){
+function addingProfile(e) {
   e.preventDefault();
 
   let person = {
-    name :document.getElementById(`name`).value,
-    city :document.getElementById(`city`).value,
-    country :document.getElementById(`country`).value,
-    age :document.getElementById(`age`).value
-    };
+    name: document.getElementById(`name`).value,
+    city: document.getElementById(`city`).value,
+    country: document.getElementById(`country`).value,
+    age: document.getElementById(`age`).value
+  };
 
-   data.push(
+  data.push(
     person
-       );
+  );
 
-    addRow(person);
+  if (rowNumber && (rowNumber % 10) == 0) {
+    pageNumber += 1;
+    tbodyElement.innerHTML = ``;
   }
 
- function addRow (r){    
 
-  tbodyElement.innerHTML+=`
+  rowNumber += 1;
+
+  addRow(person);
+  
+  handleArrowVisibility();
+}
+
+function handleArrowVisibility()
+{
+    if(pageNumber == 0){
+      prePage.classList.remove("active");
+      prePage.classList.add("disabled");
+
+      if((data.length / 10) > 1){
+        nexPage.classList.remove("disabled");
+        nexPage.classList.add("active");
+      }
+      else
+      {
+        nexPage.classList.remove("active");
+        nexPage.classList.add("disabled");
+      }
+    }
+    else {    
+      prePage.classList.add("active");
+      prePage.classList.remove("disabled");
+
+      console.log(parseInt(data.length / 10));
+      console.log(pageNumber);
+
+      if(parseInt(data.length / 10) > pageNumber){
+        nexPage.classList.add("active");
+        nexPage.classList.remove("disabled");
+      }
+      else {
+        nexPage.classList.remove("active");
+        nexPage.classList.add("disabled");
+      }
+    }
+}
+
+function preClick() {
+  pageNumber -= 1;
+
+  tbodyElement.innerHTML = ``
+
+  console.log(pageNumber);
+  let startIndex = pageNumber  * 10;
+
+  rowNumber = startIndex;
+
+  for (let i = startIndex; i < startIndex + 10; i++) {
+    if (i >= 0) {
+      rowNumber += 1;
+      addRow(data[i]);
+    }
+  }
+
+  handleArrowVisibility();
+}
+
+function nexClick() {
+  pageNumber += 1;
+  tbodyElement.innerHTML = ``;
+  let startIndex = pageNumber * 10;
+
+  rowNumber = startIndex;
+
+  for (let i = startIndex; i < startIndex + 10; i++) {
+    if (i < data.length) {
+      rowNumber += 1;
+      addRow(data[i]);
+    }
+  }
+
+  handleArrowVisibility();
+}
+
+
+
+function addRow(r) {
+  tbodyElement.innerHTML += `
       <tr>
       <td style="width:5%"> ${rowNumber} </td>
       <td style="width:20%">${r.name} </td>
@@ -146,43 +229,39 @@ function addingProfile(e){
       <td  style="width:3%"><button class="EditBtn" style="border:none" >ویرایش</button></td>
       </tr>  
   `;
-  rowNumber+=1;
 }
 
-function deleteRow(e){
-  if(!e.target.classList.contains("DeleteBtn")){
+function deleteRow(e) {
+  if (!e.target.classList.contains("DeleteBtn")) {
     return;
   }
   e.target.closest("tr").remove();
 
 }
-function editRow(e){
-  if(!e.target.classList.contains("EditBtn")){
+
+function editRow(e) {
+  if (!e.target.classList.contains("EditBtn")) {
     return;
-  } 
-  else if (e.target.textContent==="ویرایش"){
-  document.getElementById(`name`).value= e.target.closest("tr").children[1].innerHTML;
-  document.getElementById(`city`).value=e.target.closest("tr"). children[2].innerHTML;
-  document.getElementById(`country`).value=e.target.closest("tr").children[3].innerHTML;
-  document.getElementById(`age`).value=e.target.closest("tr").children[4].innerHTML;
-  e.target.textContent="ذخیره";
-  }
-  else if (e.target.textContent==="ذخیره"){
-  e.target.closest("tr").children[1].innerHTML=  document.getElementById(`name`).value;
-  e.target.closest("tr"). children[2].innerHTML= document.getElementById(`city`).value;
-  e.target.closest("tr").children[3].innerHTML=document.getElementById(`country`).value;
-  e.target.closest("tr").children[4].innerHTML=document.getElementById(`age`).value;
-  e.target.textContent="ویرایش";
+  } else if (e.target.textContent === "ویرایش") {
+    document.getElementById(`name`).value = e.target.closest("tr").children[1].innerHTML;
+    document.getElementById(`city`).value = e.target.closest("tr").children[2].innerHTML;
+    document.getElementById(`country`).value = e.target.closest("tr").children[3].innerHTML;
+    document.getElementById(`age`).value = e.target.closest("tr").children[4].innerHTML;
+    e.target.textContent = "ذخیره";
+  } else if (e.target.textContent === "ذخیره") {
+    e.target.closest("tr").children[1].innerHTML = document.getElementById(`name`).value;
+    e.target.closest("tr").children[2].innerHTML = document.getElementById(`city`).value;
+    e.target.closest("tr").children[3].innerHTML = document.getElementById(`country`).value;
+    e.target.closest("tr").children[4].innerHTML = document.getElementById(`age`).value;
+    e.target.textContent = "ویرایش";
   }
 
 
 }
 
 
-formElement.addEventListener("submit",addingProfile);
-tableElement.addEventListener("click", deleteRow );
-tableElement.addEventListener("click", editRow );
-
-
-
-
+formElement.addEventListener("submit", addingProfile);
+tableElement.addEventListener("click", deleteRow);
+tableElement.addEventListener("click", editRow);
+prePage.addEventListener("click", preClick);
+nexPage.addEventListener("click", nexClick);
